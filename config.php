72e26c2831e1c49388439242b6af314bcd44fe9c
Muhammad Risky <56730687+riskyfreak@users.noreply.github.com>
2019-11-03 14:46:28 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+///RememberWeb jika tidak ada kosongkan saja
+
+$UserAgent = "xxxxxxxxx";
+$CsrfToken = "xxxxxxxxx";
+$XSRFTOKEN = "xxxxxxxxx";
+$AdDogeSession = "xxxxxxxx";
+$RememberWeb = "xxxxxxxx";
